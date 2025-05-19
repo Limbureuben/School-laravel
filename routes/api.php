dd282db5a025cb855248f130cfac1c3de9d857ee
Limbureuben <limbureuben4@gmail.com>
2025-05-19 22:53:48 +0300

    changes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\Api\StudentController;
 
 Route::post('/students', [StudentController::class, 'store']);
