565a6ed834b8ef208841b70e519cc4a0f929d530
Limbureuben <limbureuben4@gmail.com>
2025-05-21 22:09:15 +0300

    changes

--- app/Http/Controllers/Api/StudentRegistrationController.php
+++ app/Http/Controllers/Api/StudentRegistrationController.php
@@ -15,6 +15,7 @@ class StudentRegistrationController extends Controller
         $request->validate([
             'first_name' => 'required|string',
             'last_name' => 'required|string',
+            'gender' => 'required|string',
         ]);
 
         do {
@@ -24,6 +25,7 @@ class StudentRegistrationController extends Controller
         $student = StudentRegistration::create([
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
+            'gender' => $request->gender,
             'reg_no' => $regNo,
             'password' => Hash::make($request->last_name),
         ]);
