13b4efbf0652262fc1e4a2f11ae8b61af38ee508
Limbureuben <limbureuben4@gmail.com>
2025-05-24 21:19:45 +0300

    changes

--- app/Http/Controllers/Api/UserRegisterCntroller.php
+++ app/Http/Controllers/Api/UserRegisterCntroller.php
@@ -6,5 +6,30 @@ use Illuminate\Http\Request;
 
 class UserRegisterCntroller extends Controller
 {
-    //
+    public function user_registration(Request $request) {
+
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password'=> 'required|min:6',
+            'role'=> 'required|in:teacher, staff'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' =>false,
+                'message' => $validator->errors()->first
+            ], 422);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+
+        ]);
+
+        return response()->json(['message' => 'User registered successfully!', 'user' => $user], 201);
+    }
 }
