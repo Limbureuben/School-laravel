b1a70665be2722d10f65aa56ce287e626a3d194d
Limbureuben <limbureuben4@gmail.com>
2025-05-24 21:42:29 +0300

    changes

--- app/Http/Controllers/Api/UserRegisterController.php
+++ app/Http/Controllers/Api/UserRegisterController.php
@@ -1,10 +1,11 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
-
-class UserRegisterCntroller extends Controller
+class UserRegisterController extends Controller
 {
     public function user_registration(Request $request) {
 
