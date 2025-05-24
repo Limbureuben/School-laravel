cd229e568fc28c77586473e964d35bc4c98e1e4a
Limbureuben <limbureuben4@gmail.com>
2025-05-24 21:24:55 +0300

    changes

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\StudentController;
 use App\Http\Controllers\Api\StudentRegistrationController;
+use App\Http\Controllers\Api\UserRegisterController;
 
 Route::post('/students', [StudentController::class, 'store']);
 Route::post('/register', [StudentRegistrationController::class, 'register']);
 Route::post('/login', [StudentRegistrationController::class, 'login']);
+Route::post('/user-registration', [UserRegisterController::class, 'user_registration']);
